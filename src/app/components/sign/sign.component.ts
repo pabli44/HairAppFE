37f23272393e37a5b8dafb74d9889341e8df2e7a
pabli44 <pablivg@gmail.com>
2019-08-04 17:46:02 -0500

    Sign In form and formGroup and formControl using

--- src/app/components/sign/sign.component.ts
+++ src/app/components/sign/sign.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
     selector: 'sign',
@@ -8,4 +9,30 @@ import { Component } from '@angular/core';
 
 export class SignComponent{
     title = "Sign Page";
+
+    signForm = new FormGroup({
+        name: new FormControl(''),
+        lastName: new FormControl(''),
+        phone: new FormControl(''),
+        email: new FormControl(''),
+        profileP: new FormControl(''),
+        profileC: new FormControl('')
+    });
+
+    onSubmit() {
+        // TODO: Use EventEmitter with form value
+        console.log(this.signForm.get('name').value);
+    }
+
+    checkProfile = e => {
+        console.log(e.target.checked);
+    }
+
+    constructor(){
+        this.signForm.get('name').setValue("Cambios");
+        console.log(this.signForm.get('name').value);
+        
+        console.log(this.signForm.get('profileP'));
+        console.log(this.signForm.get('profileC'));
+    }
 }
