567200326e99e60c314fa56daa6bbdcd73fc5137
pabli44 <pablivg@gmail.com>
2024-04-01 22:19:34 -0500

    updates

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { Routes, RouterModule } from '@angular/router';
 
 //import components
 import { HomeComponent } from './components/home/home.component';
-import { ContactComponent } from './components/contact/contact.component'; 
+import { ContactComponent } from './components/contact/contact.component';
 import { RegisterComponent } from './components/register/register.component';
 import { AboutComponent } from './components/about/about.component';
 import { LoginComponent } from './components/login/login.component';
@@ -34,4 +34,4 @@ const routes: Routes = [
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
-export const routing: ModuleWithProviders = RouterModule.forRoot(routes);
+export const routing: ModuleWithProviders<any> = RouterModule.forRoot(routes);
